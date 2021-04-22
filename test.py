efd49c9efa79df941d933aaf658bd7098030eaf9
David Papworth <dpapworth@academytrainee.com>
2021-04-22 15:13:05 +0000

    finished task 2

--- test.py
+++ test.py
@@ -9,9 +9,27 @@ print(f'{name} you will turn 100 in the year {year}')
 
 number = int(input('Enter a number: '))
 solve = number % 2
+solve_2 = number % 4
 
 if solve == 0:
     print('the number is even')
 
 else:
     print('the number is odd')
+
+if solve_2 == 0:
+    print('this number is divisible by 4')
+
+else:
+    print('this number is not divisible by 4')
+
+num = int(input('Enter a numerator: '))
+check = int(input('Enter a denominator: '))
+
+solve_3 = num % check
+
+if solve_3 == 0:
+    print('the division is possible')
+
+else:
+    print('the division is not possible')
