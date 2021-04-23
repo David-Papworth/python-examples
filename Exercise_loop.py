ba822fa57da1cb96a726985fd922ea6581a5b9f5
David Papworth <dpapworth@academytrainee.com>
2021-04-23 09:23:44 +0000

    loop excercise added

---
+++ Exercise_loop.py
@@ -0,0 +1,17 @@
+count = 0
+
+while count < 1:
+    name = str(input("What is your name person 1?"))
+    name_1 = str(input("What is your name person 2?"))
+    name_2 = str(input("What is your name person 3?"))
+    name_3 = str(input("What is your name person 4?"))
+    name_4 = str(input("What is your name person 5?"))
+    print(name, "is awesome!")
+    print(name_1, "is awesome!")
+    print(name_2, "is awesome!")
+    print(name_3, "is awesome!")
+    print(name_4, "is awesome!")
+    count += 1
+
+for i in range(10,21,2):
+    print(i)
