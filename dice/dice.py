057efa26620877b32e70bbf1c79dce0c7e768a8e
David Papworth <dpapworth@academytrainee.com>
2021-04-26 13:57:00 +0000

    complete dice module

---
+++ dice/dice.py
@@ -0,0 +1,4 @@
+from dice_1.dice_1 import roll_dice
+
+print (f"result for roll 1 is {roll_dice()}")
+print (f"result for roll 2 is {roll_dice()}") 
---
+++ dice/dice_1/dice_1.py
@@ -0,0 +1,4 @@
+from random import randint
+
+def roll_dice():
+    return randint(1,6)
