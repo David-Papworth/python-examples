36f0ba26ef7474ac89c8be37c3e1488e9437bdd8
David Papworth <dpapworth@academytrainee.com>
2021-04-26 16:51:28 +0000

    monday problem atempt

---
+++ Monday_problem/monday_problem.py
@@ -0,0 +1,13 @@
+even_numbers = []
+def even_between (number_lower, number_upper):
+    count = number_upper
+    while count >= number_lower and count <= number_upper:
+        if (int(count // 1000)) % 2 == 0:
+            if (int(count // 100)) % 2 == 0:
+                if (int(count // 10)) % 2 == 0:
+                    if count % 2 == 0:
+                        even_number.append(count)
+        count -= 1
+    return even_numbers
+
+print(even_between(10, 30))
---
+++ Monday_problem/test.py
@@ -0,0 +1,8 @@
+even_number = []
+count = 22
+if (int(count // 1000)) % 2 == 0:
+            if (int(count // 100)) % 2 == 0:
+                if (int(count // 10)) % 2 == 0:
+                    if count % 2 == 0:
+                        even_number.append(count)
+                        print(even_number)
